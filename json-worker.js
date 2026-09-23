81206866c5a1d33ea5c13bb9abb2a7af3ba07aca
SaifEldinRyha <syfaldynmhsn3@gmail.com>
2026-09-23 14:18:03 +0300

    Delete json-worker.js

--- json-worker.js
+++
@@ -1,8 +0,0 @@
-self.onmessage = (event) => {
-  try {
-    const result = JSON.parse(event.data.text);
-    self.postMessage({ id: event.data.id, result });
-  } catch (error) {
-    self.postMessage({ id: event.data.id, error: error.message || String(error) });
-  }
-};
